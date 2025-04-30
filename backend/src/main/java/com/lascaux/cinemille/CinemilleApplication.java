14aaac9f48a6b25dcbda241d85c92f5c66f5e588
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2025-04-30 18:00:34 +0200

    Scaffold iniziale del progetto con Angular 19 e Spring Boot.

---
+++ backend/src/main/java/com/lascaux/cinemille/CinemilleApplication.java
@@ -0,0 +1,13 @@
+package com.lascaux.cinemille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemilleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lascaux/cinemille/CinemilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lascaux.cinemille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
